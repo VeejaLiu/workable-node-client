ff2b064beaa1795be9f90a558c32bfa244a89862
Veeja Liu <veejaliu@gmail.com>
2023-02-09 10:33:07 +0800

    Add interface

---
+++ src/interface/Recruiter.ts
@@ -0,0 +1,5 @@
+export interface Recruiter {
+    id: string; // The recruiter identifier
+    name: string; // The full name of the recruiter
+    email: string; // The email address of the recruiter
+}
---
+++ src/interface/Stage.ts
@@ -0,0 +1,14 @@
+export interface Stage {
+    slug: string; // The unique token of the stage
+    name: string; // The stage name
+    kind:
+        | 'sourced'
+        | 'applied'
+        | 'shortlisted'
+        | 'assessment'
+        | 'phone-screen'
+        | 'interview'
+        | 'offer'
+        | 'hired'; // The type of the stage. Possible values are sourced, applied, shortlisted, assessment, phone-screen, interview, offer & hired
+    position: number; // The position of the stage in the pipeline (zero based)
+}
